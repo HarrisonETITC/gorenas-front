cd243a02044bab869f88fe2baf0a766604f5a070
HarrisonETITC <127680516+HarrisonETITC@users.noreply.github.com>
2025-08-31 17:59:30 -0500

    Cambiado manejador de paquetes a pnpm e iniciado script para modularización

---
+++ modularize.sh
@@ -0,0 +1,24 @@
+nx init
+
+#domain
+nx generate @nx/js:library domain --directory=libs --buildable=true --publishable=false --unitTestRunner=jest --bundler=tsc --importPath=@gorenas/domain
+
+#application
+nx generate @nx/js:library application-core --directory=libs/application --buildable=true --publishable=false --unitTestRunner=jest --bundler=tsc --importPath=@gorenas/application-core
+nx generate @nx/angular:library name=application-angular --directory=libs/application --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/application-angular
+
+# Shared
+nx generate @nx/js:library shared-util-core --directory=libs/shared/util --buildable=true --publishable=false --unitTestRunner=jest --bundler=tsc --importPath=@gorenas/shared-util-core
+nx generate @nx/angular:library --name=shared-util-forms --directory=libs/shared/util --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/shared-util-forms
+
+# Data Access
+nx generate @nx/angular:library --name=data-access-core --directory=libs/data-access --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/data-access-core
+nx generate @nx/angular:library --name=data-access-features --directory=libs/data-access --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/data-access-features
+nx generate @nx/angular:library --name=data-access-forms --directory=libs/data-access --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/data-access-forms
+nx generate @nx/angular:library --name=data-access-commons --directory=libs/data-access --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/data-access-commons
+
+# UI
+nx generate @nx/angular:library --name=ui-commons --directory=libs/ui --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/ui-commons
+nx generate @nx/angular:library --name=ui-controls --directory=libs/ui --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/ui-controls
+nx generate @nx/angular:library --name=ui-forms --directory=libs/ui --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/ui-forms
+nx generate @nx/angular:library --name=ui-features --directory=libs/ui --buildable=true --publishable=false --unitTestRunner=jest --routing=false --standalone=true --importPath=@gorenas/ui-features
