c2e980862d2a6e54e206672d6ce8889afe42e163
Harrison237 <harrisonramirez237@hotmail.com>
2026-01-05 00:00:15 -0500

    corrección en endpoint de backend

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -2,7 +2,7 @@ import { API_URL_TOKEN, STORAGE_TYPE_TOKEN } from '@gorenas/application-core';
 import { Provider } from '@angular/core';
 
 const localUrl= 'http://localhost:3000';
-const productionUrl= 'https://web-22w8kdao0i7q.up-de-fra1-k8s-1.apps.run-on-seenode.com';
+const productionUrl= 'https://web-k9llcu4rrz01.up-de-fra1-k8s-1.apps.run-on-seenode.com';
 
 export const APP_CONFIG_PROVIDERS: Provider[] = [
   {
